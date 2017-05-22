5feadd8ec475e92276b14e0c484807df93007673
Patrick Martin <patrickmmartin@gmail.com>
2017-05-22 18:09:13 +0100

    signed overflow warning

--- tests/tools/bound.cpp
+++ tests/tools/bound.cpp
@@ -125,7 +125,7 @@ int main(int argc, char *argv[])
     cdb2_bind_param(db2, "ba", CDB2_BLOB, &ba, sizeof(ba));
     add_param(sp, sql, types, "ba", "hex(", ")");
 
-    int forblob[] = {htonl(0xdeadbeef), htonl(0xcafebabe), htonl(0xffffffff)};
+    unsigned int forblob[] = {htonl(0xdeadbeef), htonl(0xcafebabe), htonl(0xffffffff)};
     cdb2_bind_param(db1, "blob", CDB2_BLOB, forblob, sizeof(forblob));
     cdb2_bind_param(db2, "blob", CDB2_BLOB, forblob, sizeof(forblob));
     add_param(sp, sql, types, "blob", "hex(", ")");
